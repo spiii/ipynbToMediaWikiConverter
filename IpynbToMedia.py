4ca50a511eb1731807daf1d15827e2b2dbe3ae2e
spiii <ghdc@gmx.de>
2017-08-01 22:07:02 +0200

    init

---
+++ IpynbToMedia.py
@@ -0,0 +1,41 @@
+from os import listdir
+from os.path import isfile, join
+from nbconvert import HTMLExporter
+ 
+inputDir= "ipynbFiles\\"
+outputDir= "htmlFiles\\"
+
+
+
+# Converts ipynb file to Html.
+def convertToHtml(filePath):
+    html_exporter = HTMLExporter()
+    html_exporter.template_file = 'basic'
+ 
+    (body, resources) = html_exporter.from_filename(filePath)
+    return body
+ 
+# Filter files from input dir.
+onlyFiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
+  
+# filter html files
+ipynbFiles = [f for f in onlyFiles if f.find('ipynb')>0]
+ 
+mediawikiContent=""
+
+for ipynbFile in ipynbFiles:
+    filePath = inputDir + ipynbFile
+    fileOutPath = outputDir + ipynbFile.replace('ipynb','html')
+ 
+    myHtml=convertToHtml(filePath)
+    mediawikiContent += '='+ipynbFile+'='
+    file = open(fileOutPath,"w") 
+    htmlText = '<html>{0}</html>'.format(myHtml)
+    file.write( htmlText)
+    mediawikiContent += '\n{0}\n'.format(htmlText)
+    file.close() 
+ 
+# Prepare one mediawiki file for all ipynb.
+file = open(outputDir+ "wiki.txt","w") 
+file.write(mediawikiContent)
+file.close()
---
+++ README.md
@@ -0,0 +1,20 @@
+# ipynbToMediaWikiConverter
+Work around to convert the ipynb files to mediawiki format
+
+It´s easy way to convert a lot of ipynb files into (html)mediawiki format.
+
+
+## Steps
+
+1. Set the notebook Css in your MediaWiki
+
+Get css from your ipynb file
+Just copy the css part oft the source code of your ipynb file.
+
+Set this css to your mediawiki css. To do this, edit this page:
+https://www.yourWikiUrl.com/wiki/index.php/MediaWiki:Common.css
+
+2. Check out this project and put your ipynb Files into the "ipynbFiles" folder.
+3. Start the IpynbToMedia.py
+4. Get your html files from "htmlFiles" folder.
+The wiki.txt file contains all the convertet contents ready for copy and paste into Your wiki.
